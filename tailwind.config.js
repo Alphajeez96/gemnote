8c373c273ada63dad96a5bccc68d736727c5b204
Prince <chimeremezechukwudire@gmail.com>
2021-03-23 02:13:47 +0100

    recent additions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-    purge: {
-        enabled: process.env.NODE_ENV === 'production',
-        content: [
-            'components/**/*.vue',
-            'plugins/**/*.js',
-        ]
-    },
+
     darkMode: "class", // or 'media' or 'class'
     theme: {
         extend: {},
